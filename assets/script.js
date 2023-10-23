7c7b7572c3a0f3c769297201715a2ad47781e946
cbrewer <144485839+VWbrewer@users.noreply.github.com>
2023-10-23 10:10:42 -0500

    Added event listener

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,23 @@
 var today = dayjs();
 $('#today').text(today.format('MMM D, YYYY'));
 
-
-
-
-
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  
+  document.querySelectorAll(".saveBtn").forEach(elem => elem.addEventListener("click", saveText));
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
